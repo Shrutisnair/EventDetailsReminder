24bc07adca979792135917ba70cab6c051434cde
Shruthi <shruthi@Shruthis-Mac-Pro.local>
2015-07-15 13:31:40 +0530

    modifying detailsviewco ntroller

--- Sample/DetailsViewController.m
+++ Sample/DetailsViewController.m
@@ -7,12 +7,24 @@
 //
 
 #import "DetailsViewController.h"
-#import "PlaceAnnotation.h"
+//#import "PlaceAnnotation.h"
 
+@interface PlaceAnnotation : NSObject <MKAnnotation>
+
+@property (nonatomic, assign) CLLocationCoordinate2D coordinate;
+@property (nonatomic, retain) NSString *title;
+@property (nonatomic, retain) NSString *subTitle;
+@property (nonatomic, retain) NSURL *url;
+
+@end
 @interface DetailsViewController ()
 
 @property (nonatomic, strong) PlaceAnnotation *annotation;
 
+@property (nonatomic, assign) MKCoordinateRegion boundingRegion;
+
+@property (nonatomic, strong) MKLocalSearch *localSearch;
+@property (nonatomic, strong) CLLocationManager *locationManager;
 @end
 
 
@@ -67,9 +79,87 @@
 }
 */
 
-- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+/*- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
 {
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
     [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+}*/
+
+-(void)viewDidAppear:(BOOL)animated
+ {
+     [super viewDidAppear:animated];
+     
+     [self.mapView setRegion:self.boundingRegion];
+     
+     if (self.mapItemList.count == 1)
+     {
+         MKMapItem *mapItem = [self.mapItemList objectAtIndex:0];
+         
+         self.title = mapItem.name;
+         
+         // add the single annotation to our map
+         PlaceAnnotation *annotation = [[PlaceAnnotation alloc] init];
+         annotation.coordinate = mapItem.placemark.location.coordinate;
+         annotation.title = mapItem.name;
+         annotation.url = mapItem.url;
+         [self.mapView addAnnotation:annotation];
+         
+         // we have only one annotation, select it's callout
+         [self.mapView selectAnnotation:[self.mapView.annotations objectAtIndex:0] animated:YES];
+         
+         // center the region around this map item's coordinate
+         self.mapView.centerCoordinate = mapItem.placemark.coordinate;
+     }
+     else
+     {
+         self.title = @"All Places";
+         
+         // add all the found annotations to the map
+         for (MKMapItem *item in self.mapItemList)
+         {
+             PlaceAnnotation *annotation = [[PlaceAnnotation alloc] init];
+             annotation.coordinate = item.placemark.location.coordinate;
+             annotation.title = item.name;
+             annotation.url = item.url;
+             [self.mapView addAnnotation:annotation];
+             
+             
+         }
+     }
+     
+ }
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    [self.mapView removeAnnotations:self.mapView.annotations];
 }
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+        return UIInterfaceOrientationMaskAll;
+    else
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+}
+
+
+#pragma mark - MKMapViewDelegate
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation
+{
+    MKPinAnnotationView *annotationView = nil;
+    if ([annotation isKindOfClass:[PlaceAnnotation class]])
+    {
+        annotationView = (MKPinAnnotationView *)[self.mapView dequeueReusableAnnotationViewWithIdentifier:@"Pin"];
+        if (annotationView == nil)
+        {
+            annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+            annotationView.canShowCallout = YES;
+            annotationView.animatesDrop = YES;
+        }
+    }
+    return annotationView;
+}
+
 @end
