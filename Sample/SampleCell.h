fd7a13372034888785673b8c34f01ce24a88e02d
Shruthi <shruthi@Shruthis-Mac-Pro.local>
2015-07-24 17:31:56 +0530

    notification for events

--- Sample/SampleCell.h
+++ Sample/SampleCell.h
@@ -15,5 +15,9 @@
     @property (nonatomic, weak) IBOutlet UILabel *timeLabel;
     @property (nonatomic, weak) IBOutlet UILabel *dateLabel;
     @property (nonatomic, weak) IBOutlet UILabel *locationLabel;
-    @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+    @property (weak, nonatomic) IBOutlet UIImageView *star1;
+    @property (weak, nonatomic) IBOutlet UIImageView *star2;
+    @property (weak, nonatomic) IBOutlet UIImageView *star3;
+    @property (weak, nonatomic) IBOutlet UIImageView *star4;
+    @property (weak, nonatomic) IBOutlet UIImageView *star5;
 @end
--- Sample/SampleCell.m
+++ Sample/SampleCell.m
@@ -14,5 +14,10 @@
     @synthesize dateLabel=_dateLabel;
     @synthesize timeLabel=_timeLabel;
     @synthesize locationLabel=_locationLabel;
-    @synthesize imageView=_imageView;
-   @end
+    @synthesize star1=_star1;
+    @synthesize star2=_star2;
+    @synthesize star3=_star3;
+    @synthesize star4=_star4;
+    @synthesize star5=_star5;
+
+@end
