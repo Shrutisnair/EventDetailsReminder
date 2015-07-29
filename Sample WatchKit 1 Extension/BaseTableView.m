9a924bd3a6e9034491c82edc74481702a15ca8d4
Shruthi <shruthi@Shruthis-Mac-Pro.local>
2015-07-29 17:17:30 +0530

    event table in watch

---
+++ Sample WatchKit 1 Extension/BaseTableView.m
@@ -0,0 +1,16 @@
+//
+//  BaseTableView.m
+//  Sample
+//
+//  Created by Shruthi on 29/07/15.
+//  Copyright © 2015 Shruthi. All rights reserved.
+//
+
+#import "BaseTableView.h"
+#import <WatchKit/WatchKit.h>
+
+@implementation BaseTableView
+
+@synthesize label;
+
+@end
