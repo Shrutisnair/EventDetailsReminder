148af6869f4297cb11149de6207dc32439c9eae2
Shruthi <shruthi@Shruthis-Mac-Pro.local>
2015-07-29 17:17:08 +0530

    event table in watch

---
+++ Sample WatchKit 1 Extension/BaseTableView.h
@@ -0,0 +1,18 @@
+//
+//  BaseTableView.h
+//  Sample
+//
+//  Created by Shruthi on 29/07/15.
+//  Copyright © 2015 Shruthi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <WatchKit/WatchKit.h>
+
+@interface BaseTableView : NSObject
+
+
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel* label;
+
+
+@end
