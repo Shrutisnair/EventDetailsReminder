cb75dc0677dc1cbc5dcedbf3dad9335818ef2f9e
Shruthi <shruthi@Shruthis-Mac-Pro.local>
2015-07-29 17:18:07 +0530

    event table in watch

--- Sample WatchKit 1 Extension/InterfaceController.m
+++ Sample WatchKit 1 Extension/InterfaceController.m
@@ -7,7 +7,7 @@
 //
 
 #import "InterfaceController.h"
-
+#import "BaseTableView.h"
 
 @interface InterfaceController()
 
@@ -15,6 +15,8 @@
 
 
 @implementation InterfaceController
+
+@synthesize table;
 
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
@@ -25,12 +27,56 @@
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
     [super willActivate];
+    [self fetchFromJson];
 }
+
+-(void)fetchFromJson {
+    
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"watchevents" ofType:@"json"];
+    
+    NSString* jsonString = [[NSString alloc] initWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    
+    event=[[NSMutableArray alloc]init];
+    
+    NSData* jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSError *jsonError;
+    id allKeys = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&jsonError];
+    
+    
+    NSMutableArray *dataObjects = [[NSMutableArray alloc] init];
+    for (int i=0; i<[allKeys count]; i++) {
+        NSDictionary *arrayResult = [allKeys objectAtIndex:i];
+        
+        [dataObjects addObject:[arrayResult valueForKey:@"eventName"]];
+          }
+    
+    [self configureTableWithData:dataObjects];
+}
+
+
+- (void)configureTableWithData:(NSMutableArray*)dataObjects {
+  //  NSLog(@"%@",[dataObjects objectAtIndex:i]);
+    
+        [self.table setNumberOfRows:[dataObjects count] withRowType:@"row"];
+    for (NSInteger i = 0; i < self.table.numberOfRows; i++) {
+        NSLog(@"%@",[dataObjects objectAtIndex:i]);
+       BaseTableView* theRow = [self.table rowControllerAtIndex:i];
+        
+     //  BaseTableView* dataObj = [dataObjects objectAtIndex:i];
+        
+       [theRow.label setText:[dataObjects objectAtIndex:i]];
+        
+      //  [theRow.rowIcon setImage:dataObj.image];
+    }
+}
+
 
 - (void)didDeactivate {
     // This method is called when watch view controller is no longer visible
     [super didDeactivate];
 }
+
 
 @end
 
